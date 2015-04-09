f4488c33f2f5e0b8db53ee92e70604bb372b0bcc
Evgeny Rusak <kasured@gmail.com>
2015-04-09 18:27:54 +0300

    [akka-cluster][shutdown hook added and some code neatness]

--- src/main/scala/com/kasured/akka_cluster/Bootstrap.scala
+++ src/main/scala/com/kasured/akka_cluster/Bootstrap.scala
@@ -17,48 +17,49 @@ object Bootstrap {
   lazy val log = org.slf4j.LoggerFactory.getLogger(this.getClass.getName)
 
   // Load the configuration
-  val config = ConfigFactory.load()
+  val config = ConfigFactory.load
 
-  val appConfig = config.getConfig("application")
+  val appConfig = config getConfig "application"
 
-  val actorSystemName = appConfig.getString("name")
+  val actorSystemName = appConfig getString "name"
 
   // Start the Akka actor system
   val system = ActorSystem(actorSystemName, config)
 
 
   // Read cluster seed nodes from the file specified in the configuration
-  val seeds = Try(appConfig.getString("cluster.seedsFile")).toOption match {
+  val seeds = Try(appConfig getString "cluster.seedsFile").toOption match {
 
       case Some(seedsFile) =>
         // Seed file was specified, read it
-        log.info(s"seed nodes from file: $seedsFile")
+        log info s"seed nodes from file: $seedsFile"
         Source.fromFile(seedsFile).getLines().map { address =>
-          AddressFromURIString.parse(s"akka.tcp://$actorSystemName@$address")
+          AddressFromURIString parse s"akka.tcp://$actorSystemName@$address"
         }.toList
 
       case None =>
         // No seed file specified, use this node as the first seed
         log.info("no seed nodes file found, using default seeds")
-        val port = appConfig.getInt("port")
-        val localAddress = Try(appConfig.getString("host"))
+        val port = appConfig getInt "port"
+        val localAddress = Try(appConfig getString "host")
           .toOption.getOrElse(InetAddress.getLocalHost.getHostAddress)
-        List(AddressFromURIString.parse(s"akka.tcp://$actorSystemName@$localAddress:$port"))
+        List(AddressFromURIString parse s"akka.tcp://$actorSystemName@$localAddress:$port")
   }
 
 
   def main(args: Array[String]): Unit = {
-    // Join the cluster with the specified seed nodes and block until termination
-    log.info(s"Joining cluster with seed nodes: $seeds")
-    Cluster.get(system).joinSeedNodes(seeds.toSeq)
 
-    system.actorOf(Props[ClusterListener], name = "clusterListener")
-
-    system.registerOnTermination {
+    sys addShutdownHook {
+      log info "[Caught the hook and trying to shutdown]"
+      system.terminate()
       log.info("[Bye fellas]")
     }
 
-    /*system.terminate()*/
+    // Join the cluster with the specified seed nodes and block until termination
+    log info s"Joining cluster with seed nodes: $seeds"
+    Cluster get(system) joinSeedNodes(seeds.toSeq)
+
+    system actorOf(Props[ClusterListener], name = "clusterListener")
 
   }
 
